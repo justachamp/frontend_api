3fa988ca7c9b24813e0d8ea9d99fd71f84ef2540
sasha <alextar.21@gmail.com>
2018-12-06 15:12:48 +0200

    - add account, address relations

--- customate/frontend_api/serializers.py
+++ customate/frontend_api/serializers.py
@@ -63,7 +63,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'address')
+        fields = ('url', 'username', 'email', 'groups', 'address', 'account')
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -71,7 +71,7 @@ class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Address
-        fields = ('address', 'account', 'country', 'address_line_1', 'address_line_2', 'city', 'locality', 'postcode', 'user')
+        fields = ('address', 'country', 'address_line_1', 'address_line_2', 'city', 'locality', 'postcode', 'user')
 
 
 class AccountSerializer(serializers.ModelSerializer):
