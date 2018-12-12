1d3745c085022f64c4e953321de2a9ddee36a3e7
sasha <alextar.21@gmail.com>
2018-12-12 13:22:42 +0200

    - add entities to user

--- customate/customate/settings.py
+++ customate/customate/settings.py
@@ -218,7 +218,7 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '1/sec',
+        'anon': '10/sec',
         'user': '10/sec'
     },
     'SEARCH_PARAM': 'filter[search]',
