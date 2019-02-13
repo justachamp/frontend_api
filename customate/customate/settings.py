55cee0ec2c017114569321c53a430dcf7845433e
Pavel Golman <pavel.golman@postindustria.com>
2019-02-13 14:57:06 +0200

    #CUS-372 no styles in the repository for frontend api. Fixed incorrect settings

--- customate/customate/settings.py
+++ customate/customate/settings.py
@@ -331,16 +331,14 @@ CORS_ALLOW_HEADERS = default_headers + (
 )
 
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 AUTH_USER_MODEL = 'core.User'
 ALLOWED_HOSTS = ['*']
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
 )
-STATICFILES_DIRS = (STATIC_ROOT,)
 AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY
 AWS_SECRET_ACCESS_KEY = AWS_SECRET_KEY
 AWS_DEFAULT_ACL = 'public-read'
