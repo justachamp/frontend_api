e8cd50a4edd04737c177def9b991a2152d51c9e7
Pavel Golman <pavel.golman@postindustria.com>
2019-03-01 10:33:25 +0200

    #CUS-423 Datasets must be available for all (logged/not logged) users

--- customate/frontend_api/views/dataset.py
+++ customate/frontend_api/views/dataset.py
@@ -1,10 +1,13 @@
 from rest_framework_json_api.views import viewsets
-
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework import response
 from core.fields import Dataset
 
+
 class DatasetView(viewsets.ViewSet):
+
+    permission_classes = (AllowAny,)
 
     @action(methods=['GET'], detail=False)
     def all(self, request):
