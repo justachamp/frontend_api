a9ed951ad70e07ddea923eed697304b2f9d1bc07
sasha <alextar.21@gmail.com>
2019-04-25 22:10:55 +0300

    #CS-60 - payment account integration draft: add attribute id property

--- customate/frontend_api/core/resource/fields.py
+++ customate/frontend_api/core/resource/fields.py
@@ -7,7 +7,6 @@ from payment_api.core.client import Client
 from payment_api.core.resource.models import ResourceQueryset
 
 
-
 class ExternalResourceRelatedField(ExternalResourceRelatedField):
     base_url = settings.PAYMENT_API_URL
 
@@ -31,11 +30,16 @@ class ExternalResourceRelatedField(ExternalResourceRelatedField):
         included = self.get_included_resources()
         if len(included):
             queryset.including(*included)
-        attr = queryset.one(instance.payment_account_id, map_attributes=True) if instance.payment_account_id else None
+        attr_id = getattr(instance, self.attribute_identity_field, None)
+        attr = queryset.one(attr_id, map_attributes=True) if attr_id else None
         setattr(instance, self.field_name, attr)
         self.context['client'] = self.client
 
         return attr
+
+    @property
+    def attribute_identity_field(self):
+        return f'{self.field_name}_id'
 
     def get_included_resources(self):
         included_resources = utils.get_included_resources(self.context.get('request'))
