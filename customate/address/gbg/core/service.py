a6b77a12c6908900a995fda9d3af51ceecc24433
sasha <alextar.21@gmail.com>
2019-05-17 15:22:33 +0300

    CS-154 add bank account validation client for direct debit add funding source service for bank account vaildation

--- customate/address/gbg/core/service.py
+++ customate/address/gbg/core/service.py
@@ -15,6 +15,22 @@ from address.gbg.core.utils import set_value_at_keypath
 from core.models import User
 
 
+def format_country_data(identity_key, post_data=None):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(self, *args, **kwargs):
+            identity = {}
+            kwargs['identity'] = identity
+            f(self, *args, **kwargs)
+            if len(identity):
+                if post_data and len(post_data):
+                    for key, value in post_data.items():
+                        set_value_at_keypath(identity, key, value)
+            return {identity_key: identity} if len(identity) else None
+        return wrapped
+    return wrapper
+
+
 class ZeepProvider:
     _client = None
     _transport = None
@@ -56,19 +72,18 @@ class ZeepProvider:
         return self.client.bind(service, port)
 
 
-class ID3Client:
+class BaseID3Client:
     _provider = None
     _parser = None
     _service = {}
 
     def __init__(self, parser, country_code):
-        self.user_name = getattr(settings, 'GBG_ACCOUNT')
-        self.password = getattr(settings, 'GBG_PASSWORD')
-        self.wsdl = getattr(settings, 'GBG_WDSL')
-        self.profile_id = getattr(settings, f'GBG_{country_code}_PROFILE_ID', None)
-        self.profile_version = getattr(settings, 'GBG_PROFILE_VERSION')
-        self._parser = parser
+
         self.country_code = country_code
+        self._apply_profile(country_code)
+        self._apply_credentials(country_code)
+        self._parser = parser
+
         self.logger = logging.getLogger(__name__)
 
     @property
@@ -112,23 +127,35 @@ class ID3Client:
     def wsdl_dump(self):
         self.provider.client.wsdl.dump()
 
+    def _apply_profile(self, country_code):
+        raise NotImplementedError
 
-def format_country_data(identity_key, post_data=None):
-    def wrapper(f):
-        @wraps(f)
-        def wrapped(self, *args, **kwargs):
-            identity = {}
-            kwargs['identity'] = identity
-            f(self, *args, **kwargs)
-            if len(identity):
-                if post_data and len(post_data):
-                    for key, value in post_data.items():
-                        set_value_at_keypath(identity, key, value)
-            return {identity_key: identity} if len(identity) else None
+    def _apply_credentials(self, country_code):
+        raise NotImplementedError
 
-        return wrapped
 
-    return wrapper
+class ID3Client(BaseID3Client):
+
+    def _apply_profile(self, country_code):
+        self.profile_id = getattr(settings, f'GBG_{country_code}_PROFILE_ID', None)
+        self.profile_version = getattr(settings, 'GBG_PROFILE_VERSION')
+
+    def _apply_credentials(self, country_code):
+        self.user_name = getattr(settings, 'GBG_ACCOUNT')
+        self.password = getattr(settings, 'GBG_PASSWORD')
+        self.wsdl = getattr(settings, 'GBG_WDSL')
+
+
+class ID3BankAccountClient(BaseID3Client):
+
+    def _apply_profile(self, country_code):
+        self.profile_id = getattr(settings, f'GBG_{country_code}_BANK_VALIDATION_PROFILE_ID', None)
+        self.profile_version = getattr(settings, 'GBG_PROFILE_VERSION')
+
+    def _apply_credentials(self, country_code):
+        self.user_name = getattr(settings, 'GBG_BANK_VALIDATION_ACCOUNT')
+        self.password = getattr(settings, 'GBG_BANK_VALIDATION_PASSWORD')
+        self.wsdl = getattr(settings, 'GBG_BANK_VALIDATION_WDSL')
 
 
 class ModelParser(object):
@@ -256,3 +283,64 @@ class ModelParser(object):
     @format_country_data(identity_key='Spain')
     def _sp_idenity(self, source, identity):
         self.apply_if_exists(source, identity, {'TaxIDNumber.Number': 'tax_id'})
+
+
+class BankAccountParser:
+    _data = None
+
+    def __init__(self, country_code):
+        self._personal_details = None
+        self._current_address = None
+        self._banking_details = None
+        self.country_code = country_code
+
+    @property
+    def personal_details(self):
+        return self._personal_details
+
+    @personal_details.setter
+    def personal_details(self, user):
+        self._personal_details = {
+            'Forename': user.get('first_name'),
+            'Surname': user.get('last_name'),
+        }
+
+    @property
+    def banking_details(self):
+        return self._personal_details
+
+    @banking_details.setter
+    def banking_details(self, account):
+        self._banking_details = {
+            'SortCode': account.get('sortCode'),
+            'AccountNumber': account.get('accountNumber'),
+        }
+
+    @property
+    def current_address(self):
+        return self._current_address
+
+    @current_address.setter
+    def current_address(self, address):
+
+        self._current_address = {
+            'Country': address.get('country'),
+            'City': address.get('city'),
+            'ZipPostcode': address.get('postcode'),
+            'AddressLine1': address.get('address_line_1', ''),
+            'AddressLine2': address.get('address_line_2', ''),
+            'AddressLine3': address.get('address_line_3', ''),
+        }
+
+    def parse(self, source):
+        self.personal_details = source.get('recipient', {})
+        self.banking_details = source.get('account', {})
+        self.current_address = source.get('recipient', {}).get('address', {})
+
+        self._data = {
+            'Personal': {'PersonalDetails': self.personal_details},
+            'BankingDetails': {'BankAccount': self.banking_details},
+            'Addresses': {'CurrentAddress': self.current_address}
+        }
+
+        return self._data
