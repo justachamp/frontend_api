c3796babd1d73490bf35bceac69b66a15deecc19
sasha <alextar.21@gmail.com>
2019-05-27 11:52:35 +0300

    CS-148 - add validation mixin

--- customate/payment_api/services/payee.py
+++ customate/payment_api/services/payee.py
@@ -1,4 +1,4 @@
-
+from core.fields import PayeeType
 from payment_api.services import (
     BaseRequestResourceSerializerService as BaseService,
     RequestResourcePaymentAccountMixin as AccountMixin,
@@ -10,7 +10,10 @@ from payment_api.services import (
 class PayeeRequestResourceService(BaseService, AccountMixin, QuerysetMixin):
 
     def get_wallet(self, currency):
-        return self.queryset.filter(account__id=self.get_attr('payment_account_id', True), currency=currency).first()
+        return self.queryset.filter(
+            account__id=self.get_attr('payment_account_id', True),
+            type=PayeeType.WALLET.value,
+            currency=currency).first()
 
     class Meta:
         resource_name = 'payees'
