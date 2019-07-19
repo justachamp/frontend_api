3235cfaaab3bceb7da05c54541f64170974f91e0
Eugene Dymo <eugene.dymo@postindustria.com>
2019-07-19 11:27:32 +0300

    Marked test as failed. Testing Jenkins build.

--- customate/frontend_api/tests.py
+++ customate/frontend_api/tests.py
@@ -31,7 +31,8 @@ class ScheduleModelTest(SimpleTestCase):
         schedule = Schedule(fee_amount=20, deposit_amount=55, number_of_payments_left=10, payment_amount=100)
         schedule.calculate_and_set_total_sum_to_pay()
 
-        self.assertEquals(1075, schedule.total_sum_to_pay)
+        # self.assertEquals(1075, schedule.total_sum_to_pay)
+        self.fail()
 
 
 class PaymentApiClientTest(SimpleTestCase):
