f3371f4a7e578ce1c425289e0cbddc6abfbcf55e
Denis Zyazev <denis.zyazev@postindustria.com>
2019-09-25 12:21:18 +0000

    CS-676 / Non-stable phone_number type during SchedulePayment's post save event processing Approved-by: Andrew Druchenko

---
+++ customate/frontend_api/tests/test_phone_number_field.py
@@ -0,0 +1,10 @@
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestPhoneNumberField(TestCase):
+
+    def test_if_users_has_phone_number_field_as_string(self):
+        users = get_user_model().objects.all()
+        self.assertEqual(True, all([isinstance(user.phone_number, str) for user in users][:1000]))
