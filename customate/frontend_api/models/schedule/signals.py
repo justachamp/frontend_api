bf3ce644e9130d23537ebcdc1e7497a3182b5a95
deniszyazev <denis.zyazev@postindustria.com>
2019-09-25 11:44:53 +0300

    Check if users phone number is empty string

--- customate/frontend_api/models/schedule/signals.py
+++ customate/frontend_api/models/schedule/signals.py
@@ -53,12 +53,13 @@ def get_funds_senders_phones(schedule: Schedule) -> list:
     sender = schedule.origin_user  # funds sender
     senders_phone_number = sender.phone_number
     if sender.is_owner and sender.notify_by_phone:
-        return [senders_phone_number if isinstance(senders_phone_number, str)
+        return [senders_phone_number if isinstance(senders_phone_number, str) and senders_phone_number
                 else senders_phone_number.as_e164]
     # If funds sender is subuser, owner should be notified as well
     if sender.is_subuser:
         owner = sender.account.owner_account.user
-        phones = [user.phone_number if isinstance(user.phone_number, str) else user.phone_number.as_e164
+        phones = [user.phone_number if isinstance(user.phone_number, str) and user.phone_number
+                  else user.phone_number.as_e164
                   for user in [sender, owner] if user.notify_by_phone]
         return phones
     return []
