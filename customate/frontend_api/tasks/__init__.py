887e29b8fb8ead5e8bc60fcd6db8948f89e99b70
deniszyazev <denis.zyazev@postindustria.com>
2019-10-18 14:39:56 +0300

    PER_PAGE import error fixed

--- customate/frontend_api/tasks/__init__.py
+++ customate/frontend_api/tasks/__init__.py
@@ -1,12 +1,14 @@
 from .payments import make_overdue_payment, make_payment, initiate_daily_payments, on_payment_change
 from .schedules import process_unaccepted_schedules
+PER_PAGE = 5
+
 
 __all__ = [
     make_overdue_payment,
     make_payment,
     initiate_daily_payments,
     process_unaccepted_schedules,
-    on_payment_change
+    on_payment_change,
+    PER_PAGE
 ]
 
-PER_PAGE = 5
