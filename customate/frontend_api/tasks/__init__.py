fd9bb06213542852b7e26ffb54ef2e4715a82c78
deniszyazev <denis.zyazev@postindustria.com>
2019-10-18 14:30:05 +0300

    Add imports to tasks/__init__.py because celery does not recognizes it.

--- customate/frontend_api/tasks/__init__.py
+++ customate/frontend_api/tasks/__init__.py
@@ -1,1 +1,12 @@
+from .payments import make_overdue_payment, make_payment, initiate_daily_payments, on_payment_change
+from .schedules import process_unaccepted_schedules
+
+__all__ = [
+    make_overdue_payment,
+    make_payment,
+    initiate_daily_payments,
+    process_unaccepted_schedules,
+    on_payment_change
+]
+
 PER_PAGE = 5
