e9cfbb40bbdfb94d130614681d4155d86a57e452
deniszyazev <denis.zyazev@postindustria.com>
2019-10-22 20:47:59 +0300

    CS-622. Changed condition during capturing unaccepted schedules (from 'less than' to 'less than or equal')

--- customate/frontend_api/tasks/schedules.py
+++ customate/frontend_api/tasks/schedules.py
@@ -32,10 +32,10 @@ def process_unaccepted_schedules():
     # Filter opened receive funds schedules by deposit_payment_date (if not None) or start_date
     schedules_with_deposit_payment_date = opened_receive_funds_schedules.filter(
         deposit_payment_date__isnull=False).filter(
-        deposit_payment_date__lt=now.datetime)
+        deposit_payment_date__lte=now.datetime)
     schedules_without_deposit_payment_date = opened_receive_funds_schedules.filter(
         deposit_payment_date__isnull=True,
-        start_date__lt=now.datetime)
+        start_date__lte=now.datetime)
     unaccepted_schedules = schedules_with_deposit_payment_date | schedules_without_deposit_payment_date
 
     paginator = Paginator(unaccepted_schedules, settings.CELERY_BEAT_PER_PAGE_OBJECTS)
