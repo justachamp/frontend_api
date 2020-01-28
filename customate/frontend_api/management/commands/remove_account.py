34ab18d015fe63bd5e888c13bf27f9d57d0a3d9c
Eugene Dymo <eugene.dymo@postindustria.com>
2020-01-28 09:45:19 +0200

    Allow "remove accounts" command to accept list of emails

--- customate/frontend_api/management/commands/remove_account.py
+++ customate/frontend_api/management/commands/remove_account.py
@@ -6,11 +6,11 @@ from core.models import User
 
 
 class Command(BaseCommand):
-    help = "PERMANENTLY remove accounts by any related user's email. " \
+    help = "PERMANENTLY remove accounts by any related user's emails. " \
            "NOTE that payment service account will not be removed, just marked as inactive."
 
     def add_arguments(self, parser):
-        parser.add_argument('user_email', type=str, help='Email of user that exists in target account'),
+        parser.add_argument('user_emails', type=str, help='Emails of users that exist in target accounts'),
 
         parser.add_argument('--skip_confirmation',
                             type=bool, default=False,
@@ -18,79 +18,83 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        if not options.get('user_email'):
-            raise CommandError('user_email argument does not exist')
+        if not options.get('user_emails'):
+            raise CommandError('user_emails argument does not exist')
 
-        user_email = options.get('user_email')
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            self.stderr.write(f"Cannot find user record by provided email({user_email})")
-            return
+        user_emails = list(map(lambda s: s.strip(), options.get('user_emails').split(',')))
 
-        if user.is_superuser:
-            self.stderr.write("Cannot remove superuser")
-            return
+        for user_email in user_emails:
+            self.stderr.write(f"Processing email: {user_email}")
 
-        owner_account = user.account.owner_account if user.is_subuser else user.account
-        related_account_ids = [owner_account.id] + list(
-            owner_account.sub_user_accounts.all().values_list('id', flat=True)
-        )
+            try:
+                user = User.objects.get(email=user_email)
+            except User.DoesNotExist:
+                self.stderr.write(f"Cannot find user record by provided email({user_email})")
+                continue
 
-        skip_confirmation = options.get('skip_confirmation')
-        if not skip_confirmation:
-            confirm = input("Type 'yes' if you would like to permanently remove specified accounts: %s (it's time to "
-                            "check IDs and environment): "
-                            % related_account_ids)
-            if confirm != 'yes':
-                return
+            if user.is_superuser:
+                self.stderr.write("Cannot remove superuser")
+                continue
 
-        with connection.cursor() as cursor:
-            related_user_ids = []
-            cursor.execute("SELECT user_id FROM frontend_api_account WHERE id = ANY(%s)", params=[related_account_ids])
-            for user_id in cursor.fetchall():
-                related_user_ids.append(user_id[0])
+            owner_account = user.account.owner_account if user.is_subuser else user.account
+            related_account_ids = [owner_account.id] + list(
+                owner_account.sub_user_accounts.all().values_list('id', flat=True)
+            )
 
-            payment_account_ids = []
-            cursor.execute("SELECT payment_account_id FROM frontend_api_useraccount "
-                           "WHERE payment_account_id IS NOT NULL AND account_ptr_id = ANY(%s)",
-                           params=[related_account_ids])
-            for payment_account_id in cursor.fetchall():
-                payment_account_ids.append(payment_account_id[0])
-            self.stdout.write(f'Affected payment account ids: {payment_account_ids}')
+            skip_confirmation = options.get('skip_confirmation')
+            if not skip_confirmation:
+                confirm = input("Type 'yes' if you would like to permanently remove specified accounts: %s (it's time to "
+                                "check IDs and environment): "
+                                % related_account_ids)
+                if confirm != 'yes':
+                    return
 
-            self.stdout.write(f'Starting accounts removing process...')
-            cursor.execute("SET CONSTRAINTS ALL IMMEDIATE")  # Inform about constrain violation immediately
+            with connection.cursor() as cursor:
+                related_user_ids = []
+                cursor.execute("SELECT user_id FROM frontend_api_account WHERE id = ANY(%s)", params=[related_account_ids])
+                for user_id in cursor.fetchall():
+                    related_user_ids.append(user_id[0])
 
-            # Remove sub-user related records (link with owner account & permissions)
-            cursor.execute("DELETE FROM frontend_api_subuserpermission WHERE account_id = ANY(%s)",
-                           params=[related_account_ids])
-            cursor.execute("DELETE FROM frontend_api_subuseraccount WHERE owner_account_id = ANY(%s)",
-                           params=[related_account_ids])
+                payment_account_ids = []
+                cursor.execute("SELECT payment_account_id FROM frontend_api_useraccount "
+                               "WHERE payment_account_id IS NOT NULL AND account_ptr_id = ANY(%s)",
+                               params=[related_account_ids])
+                for payment_account_id in cursor.fetchall():
+                    payment_account_ids.append(payment_account_id[0])
+                self.stdout.write(f'Affected payment account ids: {payment_account_ids}')
 
-            # Remove relationship between account & company & payment account
-            cursor.execute("DELETE FROM frontend_api_useraccount WHERE account_ptr_id = ANY(%s)",
-                           params=[related_account_ids])
+                self.stdout.write(f'Starting accounts removing process...')
+                cursor.execute("SET CONSTRAINTS ALL IMMEDIATE")  # Inform about constrain violation immediately
 
-            # Remove user's accounts
-            cursor.execute("DELETE FROM frontend_api_account WHERE id = ANY(%s)", params=[related_account_ids])
+                # Remove sub-user related records (link with owner account & permissions)
+                cursor.execute("DELETE FROM frontend_api_subuserpermission WHERE account_id = ANY(%s)",
+                               params=[related_account_ids])
+                cursor.execute("DELETE FROM frontend_api_subuseraccount WHERE owner_account_id = ANY(%s)",
+                               params=[related_account_ids])
 
-            # Remove schedules related records (documents, schedule payments data and schedules themselves)
-            cursor.execute("DELETE FROM frontend_api_document WHERE user_id = ANY(%s)", params=[related_user_ids])
-            cursor.execute("DELETE FROM frontend_api_schedulepayments WHERE schedule_id "
-                           "IN (SELECT id FROM frontend_api_schedule WHERE recipient_user_id = ANY(%s) OR origin_user_id = ANY(%s))",
-                           params=[related_user_ids, related_user_ids])
-            cursor.execute(
-                "DELETE FROM frontend_api_schedule WHERE recipient_user_id = ANY(%s) OR origin_user_id = ANY(%s)",
-                params=[related_user_ids, related_user_ids])
+                # Remove relationship between account & company & payment account
+                cursor.execute("DELETE FROM frontend_api_useraccount WHERE account_ptr_id = ANY(%s)",
+                               params=[related_account_ids])
 
-            # Remove core django user related records
-            cursor.execute("DELETE FROM core_user_user_permissions WHERE user_id = ANY(%s)", params=[related_user_ids])
-            cursor.execute("DELETE FROM core_user_groups WHERE user_id = ANY(%s)", params=[related_user_ids])
-            cursor.execute("DELETE FROM core_user WHERE id = ANY(%s)", params=[related_user_ids])
+                # Remove user's accounts
+                cursor.execute("DELETE FROM frontend_api_account WHERE id = ANY(%s)", params=[related_account_ids])
 
-            # Deactivate (not REMOVE) payment account (we do this in the end, to be sure that queries pass)
-            for account_id in payment_account_ids:
-                payment_service.PaymentAccount.deactivate(payment_account_id=account_id)
+                # Remove schedules related records (documents, schedule payments data and schedules themselves)
+                cursor.execute("DELETE FROM frontend_api_document WHERE user_id = ANY(%s)", params=[related_user_ids])
+                cursor.execute("DELETE FROM frontend_api_schedulepayments WHERE schedule_id "
+                               "IN (SELECT id FROM frontend_api_schedule WHERE recipient_user_id = ANY(%s) OR origin_user_id = ANY(%s))",
+                               params=[related_user_ids, related_user_ids])
+                cursor.execute(
+                    "DELETE FROM frontend_api_schedule WHERE recipient_user_id = ANY(%s) OR origin_user_id = ANY(%s)",
+                    params=[related_user_ids, related_user_ids])
 
-            self.stdout.write(f'Done')
+                # Remove core django user related records
+                cursor.execute("DELETE FROM core_user_user_permissions WHERE user_id = ANY(%s)", params=[related_user_ids])
+                cursor.execute("DELETE FROM core_user_groups WHERE user_id = ANY(%s)", params=[related_user_ids])
+                cursor.execute("DELETE FROM core_user WHERE id = ANY(%s)", params=[related_user_ids])
+
+                # Deactivate (not REMOVE) payment account (we do this in the end, to be sure that queries pass)
+                for account_id in payment_account_ids:
+                    payment_service.PaymentAccount.deactivate(payment_account_id=account_id)
+
+                self.stdout.write(f'Done')
